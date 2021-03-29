78a7f2d4a3d9cfd879949bced80f22879d987378
Jordan <rainonej@gmail.com>
2021-03-29 01:01:28 -0400

    Found which subs trolls like to post on the most
    The subreddit_submission_dic.py file just contains a dictionary indicating the number of posts the trolls did in each subreddit.

--- target_subreddits.py
+++ target_subreddits.py
@@ -12,32 +12,67 @@ reddit = praw.Reddit(
 )
 
 from troll_list import Trolls
-subreddits = {}
-
 n = len(Trolls)
-i = 0
-for user in Trolls:
-	i += 1
-	if (i%10 == 0):
-		print('troll number', i, 'of', n)
-	troll = reddit.redditor(user)
-	try:
-		for comm in troll.comments.new(limit = 1000):
-			sub = comm.subreddit.display_name
-			if sub in subreddits:
-				subreddits[sub] += 1
-			else:
-				subreddits[sub] = 1
-	except:
-		print('Maybe a 403 error occured. We do not have access to this info')
 
-print(n, 'Trolls commented in', len(subreddits.keys()), 'unique subreddits')
-subreddits2 = {k: v for k, v in sorted(subreddits.items(), key=lambda item: -item[1])}
 
-#I already did this, no need to do it again
-#f = open("subreddi_comment_dic.py", "w")
-#f.write('target_subreddits = ' + str(subreddits2))
-#f.close()
 
-items = list(subreddits.items())
-print(items[:10])
+COMMENTS = False
+SUBMISSIONS = True
+
+if COMMENTS:
+	subreddits = {}
+	i = 0
+	for user in Trolls:
+		i += 1
+		if (i%10 == 0):
+			print('troll number', i, 'of', n)
+		troll = reddit.redditor(user)
+		try:
+			for comm in troll.comments.new(limit = 1000):
+				sub = comm.subreddit.display_name
+				if sub in subreddits:
+					subreddits[sub] += 1
+				else:
+					subreddits[sub] = 1
+		except:
+			print('Maybe a 403 error occured. We do not have access to this info')
+
+	print(n, 'Trolls commented in', len(subreddits.keys()), 'unique subreddits')
+	subreddits2 = {k: v for k, v in sorted(subreddits.items(), key=lambda item: -item[1])}
+
+	#I already did this, no need to do it again
+	#f = open("subreddit_comment_dic.py", "w")
+	#f.write('target_subreddits = ' + str(subreddits2))
+	#f.close()
+
+	items = list(subreddits.items())
+	print(items[:10])
+
+if SUBMISSIONS:
+	subreddits = {}
+	i = 0
+	for user in Trolls:
+		i += 1
+		if (i%10 == 0):
+			print('troll number', i, 'of', n)
+		troll = reddit.redditor(user)
+		try:
+			for post in troll.submissions.new(limit = 1000):
+				sub = post.subreddit.display_name
+				if sub in subreddits:
+					subreddits[sub] += 1
+				else:
+					subreddits[sub] = 1
+		except:
+			print('Maybe a 403 error occured. We do not have access to this info')
+
+	print(n, 'Trolls commented in', len(subreddits.keys()), 'unique subreddits')
+	subreddits2 = {k: v for k, v in sorted(subreddits.items(), key=lambda item: -item[1])}
+
+	#I already did this, no need to do it again
+	f = open("subreddit_submission_dic.py", "w")
+	f.write('target_post_subreddits = ' + str(subreddits2))
+	f.close()
+
+	items = list(subreddits.items())
+	print(items[:10])
